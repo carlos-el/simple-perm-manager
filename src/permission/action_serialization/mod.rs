9f9d665fefb57df440b241f620629a5ef70b0940
carlos-el <c-enriquez@outlook.com>
2021-12-03 18:09:33 +0100

    Moves deserialize_actions function to its own actions_serialization module

---
+++ src/permission/action_serialization/mod.rs
@@ -0,0 +1,47 @@
+use serde_json::{Map, Value};
+use std::collections::HashSet;
+
+//// TESTS ////
+#[cfg(test)]
+mod tests;
+
+const MAX_JSON_DEPTH_ALLOWED: u8 = 20;
+
+pub fn deserialize_actions(
+    current_depth: u8,
+    prefix: &str,
+    json_obj: &Map<String, Value>,
+) -> HashSet<String> {
+    if current_depth >= MAX_JSON_DEPTH_ALLOWED {
+        panic!("wrong format in permission json string - too much nesting")
+    }
+
+    let mut actions: HashSet<String> = HashSet::new();
+
+    for (key, value) in json_obj.into_iter() {
+        match value {
+            Value::Object(map) => {
+                actions = actions
+                    .union(&deserialize_actions(
+                        current_depth + 1,
+                        &format!("{}.{}", prefix, key),
+                        map,
+                    ))
+                    .cloned()
+                    .collect();
+            }
+            Value::Bool(val) => {
+                if *val {
+                    // Control trailing first dot with nesting counter
+                    actions.insert(format!("{}.{}", prefix, key));
+                }
+            }
+            _ => panic!(
+                "wrong format in permission json string - found no object or boolean value, {}-{}",
+                key, value
+            ),
+        };
+    }
+
+    actions
+}
--- src/permission/mod.rs
+++ src/permission/mod.rs
@@ -1,3 +1,4 @@
+mod action_serialization;
 use serde_json::{Map, Value};
 use std::collections::HashSet;
 use uuid::Uuid;
@@ -34,34 +35,6 @@ impl Permission {
     // pub fn to_str(&self) -> String;
     // pub fn to_json(&self) -> String;
 
-    // TODO control max nesting (recursivity)
-    fn deserialize_actions(prefix: &str, json_obj: &Map<String, Value>) -> HashSet<String> {
-        let mut actions: HashSet<String> = HashSet::new();
-
-        for (key, value) in json_obj.into_iter() {
-            match value {
-                Value::Object(map) => {
-                    actions = actions
-                        .union(&Permission::deserialize_actions(
-                            &format!("{}.{}", prefix, key),
-                            map,
-                        ))
-                        .cloned()
-                        .collect();
-                }
-                Value::Bool(val) => {
-                    if *val {
-                        // Control trailing first dot with nesting counter
-                        actions.insert(format!("{}.{}", prefix, key));
-                    }
-                }
-                _ => panic!("wrong format in permission json string"),
-            };
-        }
-
-        actions
-    }
-
     pub fn from_json(actions_json: &str, manager_id: &Option<Uuid>) -> Permission {
         let actions_generated: HashSet<String>;
         let actions_value: Value =
@@ -69,7 +42,7 @@ impl Permission {
 
         match actions_value {
             Value::Object(map) => {
-                actions_generated = Permission::deserialize_actions("", &map);
+                actions_generated = action_serialization::deserialize_actions(0, "", &map);
             }
             _ => panic!("wrong format in permission json string"),
         }
