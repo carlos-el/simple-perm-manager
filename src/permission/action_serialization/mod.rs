5a807c2d35b133bb2ce0f30bf8ca5c55dab33846
carlos-el <c-enriquez@outlook.com>
2021-12-10 17:50:59 +0100

    Adds function serialize_actions and fxes clippy warnings
    The serialize_actions function convers a HashSet of actions (strings) to a serde_json::Map that represents JSON.

--- src/permission/action_serialization/mod.rs
+++ src/permission/action_serialization/mod.rs
@@ -21,13 +21,13 @@ pub fn deserialize_actions(
     for (key, value) in json_obj.into_iter() {
         match value {
             Value::Object(map) => {
-                let fmt = if prefix.len() == 0 { format!("{}", key) } else { format!("{}.{}", prefix, key) };
+                let fmt = if prefix.is_empty() {
+                    key.to_string()
+                } else {
+                    format!("{}.{}", prefix, key)
+                };
                 actions = actions
-                    .union(&deserialize_actions(
-                        current_depth + 1,
-                        &fmt,
-                        map,
-                    ))
+                    .union(&deserialize_actions(current_depth + 1, &fmt, map))
                     .cloned()
                     .collect();
             }
@@ -45,4 +45,49 @@ pub fn deserialize_actions(
     }
 
     actions
+}
+
+pub fn serialize_actions(actions: &HashSet<String>) -> Map<String, Value> {
+    let mut map: Map<String, Value> = Map::new();
+    // Fo each action
+    for action in actions.iter() {
+        // Declare a mutable pointer to the map start
+        let mut map_pointer = &mut map;
+        // Get objects in an action string
+        let objects: Vec<&str> = action.split('.').collect();
+
+        // For each object
+        for obj in &objects {
+            if obj == &objects[objects.len() - 1] {
+                // If it is in the last object
+                // Add the object to the map as boolean with true as value
+                map_pointer.insert(String::from(*obj), Value::Bool(true));
+            } else if map_pointer.contains_key(&String::from(*obj)) {
+                // If it is the current map
+                // Move map pointer to the map stored for the current word
+                map_pointer = match map_pointer.get_mut(&String::from(*obj)) {
+                    Some(Value::Object(x)) => x,
+                    _ => panic!(
+                        "1unexpected error in map creation, value should had been a Value::Object"
+                    ),
+                }
+            } else {
+                // If it is NOT in the current map
+                // Add the object to the map as a new empty map
+                map_pointer.insert(String::from(*obj), Value::Object(Map::new()));
+                // Move map pointer to the map stored for the current word
+                map_pointer = match map_pointer.get_mut(&String::from(*obj)) {
+                    Some(Value::Object(x)) => x,
+                    Some(y) => panic!(
+                        "3unexpected error in map creation, value should had been a Value::Object,{}", y
+                    ),
+                    None => panic!(
+                        "4unexpected error in map creation, value should had been a Value::Object"
+                    ),
+                };
+            }
+        }
+    }
+
+    map
 }
