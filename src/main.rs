0b6360d639615da0e864d991185802cc4cbae981
carlos-el <c-enriquez@outlook.com>
2021-12-27 02:42:31 +0100

    Formats code with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -7,39 +7,39 @@ use std::collections::HashSet;
 // Excludes main from test coverage as it is just for demo purposes
 #[cfg(not(tarpaulin_include))]
 fn main() {
-  let pm = PermissionManager::from_actions(&HashSet::from([
-    String::from("building.create"),
-    String::from("building.view"),
-    String::from("building.edit"),
-    String::from("building.delete"),
-    String::from("user.create"),
-    String::from("user.view"),
-    String::from("user.edit"),
-    String::from("user.delete"),
-  ]));
+    let pm = PermissionManager::from_actions(&HashSet::from([
+        String::from("building.create"),
+        String::from("building.view"),
+        String::from("building.edit"),
+        String::from("building.delete"),
+        String::from("user.create"),
+        String::from("user.view"),
+        String::from("user.edit"),
+        String::from("user.delete"),
+    ]));
 
-  let p1 = pm.perm_from_actions(&HashSet::from([
-    String::from("building.create"),
-    String::from("building.view"),
-    String::from("building.edit"),
-  ]));
+    let p1 = pm.perm_from_actions(&HashSet::from([
+        String::from("building.create"),
+        String::from("building.view"),
+        String::from("building.edit"),
+    ]));
 
-  let p2 = pm.perm_from_actions(&HashSet::from([
-    String::from("building.edit"),
-    String::from("building.delete"),
-  ]));
+    let p2 = pm.perm_from_actions(&HashSet::from([
+        String::from("building.edit"),
+        String::from("building.delete"),
+    ]));
 
-  let p3 = pm.perm_from_actions(&HashSet::from([String::from("building.edit")]));
+    let p3 = pm.perm_from_actions(&HashSet::from([String::from("building.edit")]));
 
-  println!("Universe: {:#?}", pm.get_universe());
-  println!("Union: {:#?}", p1.union(&p2));
-  println!("Diff: {:#?}", p1.difference(&p2));
-  println!("Contains 1: {:#?}", p1.contains(&p2));
-  println!("Contains 2: {:#?}", p1.contains(&p3));
-  println!("ContAction 1: {:#?}", p1.contains_action("building.edit"));
-  println!("ContAction 2: {:#?}", p1.contains_action("building.delete"));
+    println!("Universe: {:#?}", pm.get_universe());
+    println!("Union: {:#?}", p1.union(&p2));
+    println!("Diff: {:#?}", p1.difference(&p2));
+    println!("Contains 1: {:#?}", p1.contains(&p2));
+    println!("Contains 2: {:#?}", p1.contains(&p3));
+    println!("ContAction 1: {:#?}", p1.contains_action("building.edit"));
+    println!("ContAction 2: {:#?}", p1.contains_action("building.delete"));
 
-  let data = r#"
+    let data = r#"
     {
         "building": {
           "view": true,
@@ -55,12 +55,12 @@ fn main() {
           }
     }"#;
 
-  let my_perm = Permission::from_json(data, &None);
-  println!("From JSON: {:#?}", my_perm);
-  println!("To JSON: {:#?}", my_perm.to_json());
+    let my_perm = Permission::from_json(data, &None);
+    println!("From JSON: {:#?}", my_perm);
+    println!("To JSON: {:#?}", my_perm.to_json());
 
-  let pm2 = PermissionManager::from_json(
-    r#"
+    let pm2 = PermissionManager::from_json(
+        r#"
     {
         "building": {
           "view": true,
@@ -75,15 +75,15 @@ fn main() {
             "delete": true
           }
     }"#,
-  );
+    );
 
-  let my_perm_json = pm2.perm_from_json(
-    r#"
+    let my_perm_json = pm2.perm_from_json(
+        r#"
   {
       "user": {
           "delete": true
         }
   }"#,
-  );
-  println!("From JSON 2: {:#?}", my_perm_json);
+    );
+    println!("From JSON 2: {:#?}", my_perm_json);
 }
