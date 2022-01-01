e8e942f4c3e8b5ec5bd4501a4faf5a9bc12f7d4b
carlos-el <c-enriquez@outlook.com>
2022-01-01 04:32:54 +0100

    Change lib structure and adds basic docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
-pub mod permission;
-pub mod permission_manager;
+//! Simple and flexible management and operation of permissions.
+//!
+//! Permissions can be created freely from a set of actions in order to easily operate, serialize, store or associate them.
+//! This allows fine grained customization such as per object or per subject permission association.
+//!
+//! Also, permissions can be tied to a PermissionManager with a reference permission configuration for validation, instantiation and operations between linked permissions. This potentially grants more control and security to your permission management strategy.
+
+mod permission;
+pub use permission::Permission;
+mod permission_manager;
+pub use permission_manager::PermissionManager;
