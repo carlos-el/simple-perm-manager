164c40ca54f85b2d491bdafa9b98e24cecc7bc66
carlos-el <c-enriquez@outlook.com>
2022-01-05 16:58:41 +0100

    Adds running doctest before tarpaulin coverage as tarpaulin does not execute doctests yet

--- tools/src/bin/coverage.rs
+++ tools/src/bin/coverage.rs
@@ -2,6 +2,14 @@ use std::process::Command;
 
 // Runs test and collects its coverage
 fn main() {
+    // Run only doctest as tarpaulin does not.
+    assert!(Command::new("cargo")
+        .arg("test")
+        .arg("--doc")
+        .status()
+        .expect("task coverage failed")
+        .success());
+
     // Run test and coverage extraction with tarpaulin crates package
     assert!(Command::new("cargo")
         .arg("tarpaulin")
