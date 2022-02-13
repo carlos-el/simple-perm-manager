1244eda03ad06066b38874a427e743b2ae647110
carlos-el <c-enriquez@outlook.com>
2022-02-13 12:57:15 +0100

    Adds hidden docs for Permission JSON serializing functions

--- src/permission/action_serialization/mod.rs
+++ src/permission/action_serialization/mod.rs
@@ -5,22 +5,46 @@ use std::collections::HashSet;
 #[cfg(test)]
 mod tests;
 
+// Constant for defining maximun nesting allowed in a json_object when deserializing
+// As the deserializing function is recursive, this prevent overflows.
 const MAX_JSON_DEPTH_ALLOWED: u8 = 20;
+// Contant for defining the character that acts as a divider for action subgroups in
+// serialization and deserialization.
 const ACTION_DIVIDER: char = ':';
 
+#[doc(hidden)]
+/// Deserializes a JSON object into a HashSet of string actions.
+/// The function is recursive and thus uses some parameters to control this recursivity.
+///
+/// # Arguments
+///
+/// * `current_depth` - Current recursivity depth of the function. Should be set to cero (0) everytime the function is called explicitly.
+/// Can be set to a greter value if you want to reduce max recursivity allowed by the `MAX_JSON_DEPTH_ALLOWED` constant.
+/// * `prefix` - Should be set to cero (0) everytime the function is called explicitly.
+/// Represents the action key acumulated value from previous function calls.
+/// * `json_obj` - serde_json::Map containing the JSON object with the actions that need to be deserialized into action strings.
+///
+/// # Panics
+///
+/// - Panics if `current_depth` is greater or equal to `MAX_JSON_DEPTH_ALLOWED` value.
+/// This usually means that max recursivity has been reached.
+/// - Panics if `json_obj` is not correctly formed and for any key contains a
+/// value different from a serde_json::Map or a serde_json::Bool.
 pub fn deserialize_actions(
     current_depth: u8,
     prefix: &str,
     json_obj: &Map<String, Value>,
 ) -> HashSet<String> {
+    // If we have already reached max recursivity nesting allowed then panic.
     if current_depth >= MAX_JSON_DEPTH_ALLOWED {
         panic!("wrong format in permission json string - too much nesting")
     }
 
     let mut actions: HashSet<String> = HashSet::new();
 
+    // Iterate json map object
     for (key, value) in json_obj.into_iter() {
-        // Get the string representing the action correctly formatted (Control trailing first dot with prefix value)
+        // Get the string representing the action correctly formatted (Control trailing first colon with prefix value)
         let action_value = if prefix.is_empty() {
             key.to_string()
         } else {
@@ -28,12 +52,16 @@ pub fn deserialize_actions(
         };
 
         match value {
+            // If the value for a key is a json map again then call this function recursively passing current depth
+            // and the current key value as prefix for subsequent actions.
             Value::Object(map) => {
                 actions = actions
                     .union(&deserialize_actions(current_depth + 1, &action_value, map))
                     .cloned()
                     .collect();
             }
+            // If the value is a boolean then we have reached the end of the action definition and
+            // can include the action in the actions HashSet.
             Value::Bool(val) => {
                 if *val {
                     actions.insert(action_value);
@@ -49,6 +77,8 @@ pub fn deserialize_actions(
     actions
 }
 
+#[doc(hidden)]
+/// Serializes a HashSet of string actions into a serde_json::Map which represents valid JSON.
 pub fn serialize_actions(actions: &HashSet<String>) -> Map<String, Value> {
     let mut map: Map<String, Value> = Map::new();
     // Fo each action
